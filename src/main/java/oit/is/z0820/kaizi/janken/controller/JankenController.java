9b12b8eba6a6047134d41f2ccfce303626f8be88
e1b20108 <e1b20108@oit.ac.jp>
2022-10-23 21:38:16 +0900

    Dependency Injectionを用いたオブジェクトの共有

--- src/main/java/oit/is/z0820/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z0820/kaizi/janken/controller/JankenController.java
@@ -1,11 +1,16 @@
 package oit.is.z0820.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import oit.is.z0820.kaizi.janken.model.Entry;
 
 /**
  * Sample21Controller
@@ -14,11 +19,23 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class JankenController {
+  @Autowired
+  private Entry entry;
 
   @GetMapping("/janken")
-  public String janken() {
+  public String janken(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.entry.addUser(loginUser);
+    model.addAttribute("yourName", loginUser);
+    model.addAttribute("entry", this.entry);
+
     return "janken.html";
   }
+
+  // @GetMapping("/janken")
+  // public String janken() {
+  // return "janken.html";
+  // }
 
   @PostMapping("/janken")
   public String nyusitu(@RequestParam String uname, ModelMap model) {
